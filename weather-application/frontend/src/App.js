2c0be40920a3b08b777725b619042503a67ff791
AkulaMeghanath <akulameghanath2@gmail.com>
2024-07-08 16:06:57 +0530

    login

---
+++ weather-application/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import"./App.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <br/>
+    <input placeholder="username"/>
+    <input placeholder="password" type="password" />
+    <button>Register</button>
+    <button>Login</button>
+
+
+    </div>
+    );
+  }
